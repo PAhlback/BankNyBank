a2a9d3849633c2d4002d552d978325c92b1c52d2
Balos87 <145317796+Balos87@users.noreply.github.com>
2023-11-14 10:51:17 +0100

    Update branch
    Method is done, needs implementing with menu.

--- Models/User.cs
+++ Models/User.cs
@@ -19,34 +19,56 @@ namespace BankNyBank.Models
         //Method for letting the user create a new account.
         public static void OpenNewAccount(BankContext context, User user)
         {
-            //User choice: Account name.
-            Console.WriteLine("You are about to create a new account.");
-            Console.Write("Choose your new account name: ");
-            string newAccountname = Console.ReadLine();
-            
-            //User choice: Account type.
-            Console.Write("'Salary' or 'savings' account?");
-            Console.Write("Choose account type: ");
-            string accountType = Console.ReadLine().ToLower();
+            string newAccountName = "";
+            string accountType = "";
 
-            //Put While-loop here to return an invalid option.
-            if (accountType == "salary" || accountType == "savings")
+            do
             {
+                Console.Clear();
+                Console.WriteLine("\nYou are about to create a new account.\n(Press key to continue.)");
+                Console.ReadKey();
+                Console.Write("\nChoose your new account name: ");
+                newAccountName = Console.ReadLine().ToLower();
 
-                Account newAccount = new Account()
+                if (string.IsNullOrWhiteSpace(newAccountName) )
                 {
-                    UserId = user.Id,
-                    Name = newAccountname,
-                    Balance = 0,
-                    AccountType = accountType
-                };
-                context.Accounts.Add(newAccount);
-                context.SaveChanges();
-            }
-            else
+                    Console.WriteLine("\nAccount name cannot be empty. Please try again.\n");
+                }
+            } while (string.IsNullOrWhiteSpace(newAccountName));
+
+            do
             {
-                Console.WriteLine("You did not choose a valid option.");
-            }
+                Console.Clear();
+                Console.WriteLine($"\nYour account name is {newAccountName}.\n(Press key to continue.)");
+                Console.ReadKey();
+                Console.WriteLine("\nWhat account type would you like it to be?\n'Salary' or 'savings' account?");
+                Console.Write("\nAccount type: ");
+                accountType = Console.ReadLine().ToLower();
+
+                if (accountType != "salary" && accountType != "savings")
+                {
+                    Console.WriteLine("You did not choose a valid option. Please try again.");
+                }
+            } while (accountType != "salary" && accountType != "savings");
+
+            Account newAccount = new Account()
+            {
+                UserId = user.Id,
+                Name = newAccountName,
+                Balance = 0,
+                AccountType = accountType,
+            };
+            context.Accounts.Add(newAccount);
+            context.SaveChanges();
+
+            Console.WriteLine("\n(Press key to continue)");
+            Console.ReadKey();
+
+            Console.Clear();
+            Console.WriteLine("\nYou have successfully created a new account.");
+            Console.WriteLine($"\tNew Account Information:\n\tName: {newAccountName}\n\tType: {accountType}.");
+            Console.ReadKey();
+
         }
     }
 }

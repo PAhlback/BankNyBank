1587c912e58e987166b0b8bd5ac628681a1839da
SurrealEmil <emil.ejderklev1@hotmail.com>
2023-11-21 11:40:01 +0100

    Cooldown back to start.
    Cooldown now goes back to start off program.

--- Utilites/UserAuthentication.cs
+++ Utilites/UserAuthentication.cs
@@ -19,7 +19,9 @@ namespace BankNyBank.Utilites
                 Console.Write("Enter user name: ");
                 string userName = Console.ReadLine();
 
-                User user = FindUserByName(context, userName);
+                User user = context.Users
+                .Where(u => u.Name == userName)
+                .SingleOrDefault();
 
                 if (user != null)
                 {
@@ -29,7 +31,10 @@ namespace BankNyBank.Utilites
                         return user;
                     }
 
-                    Cooldown();
+                    if (Cooldown())
+                    {
+                        break;
+                    }
 
                 }
                 else
@@ -38,6 +43,7 @@ namespace BankNyBank.Utilites
                     Console.WriteLine("No such user in the vault. Try again");
                 }
             }
+            return null;
         }
 
         // Check to see if the user that put in the name knows the corresponding pin
@@ -59,20 +65,21 @@ namespace BankNyBank.Utilites
                 }
                 else
                 {
+                    Console.Clear();
                     Console.WriteLine($"Invalid PIN. Try again. {maxLoginAttempts - i} left.");
                 }
             }
-
             return false;
         }
 
         // Method to put a user in a timeout if they input the wrong pin three times
-        private static void Cooldown()
+        private static bool Cooldown()
         {
             const int jailTime = 180; // 3 min
 
             Console.Clear();
             Console.WriteLine("You ran out of tries. Lockout timer initiated.");
+            Console.CursorVisible = false;
             for (int cooldown = jailTime; cooldown >= 0; cooldown--)
             {
                 Console.SetCursorPosition(0, 1);
@@ -81,17 +88,21 @@ namespace BankNyBank.Utilites
                 Console.WriteLine($"\nTry again in: {minutes:D2}m {remainingcooldown:D2}s");
                 Thread.Sleep(1000); // Sleep for 1 second to refresh timer properly
             }
+            Console.CursorVisible = true;
+            Console.Clear();
             Console.WriteLine("Lockout is over. Press Enter to go back to the start screen:");
             Console.ReadLine();
-            Console.Clear();
+            
+
+            return true;
         }
 
 
-        private static User FindUserByName(BankContext context, string userName)
-        {
-            return context.Users
-                .Where(u => u.Name == userName)
-                .SingleOrDefault();
-        }
+        //private static User FindUserByName(BankContext context, string userName)
+        //{
+        //    return context.Users
+        //        .Where(u => u.Name == userName)
+        //        .SingleOrDefault();
+        //}
     }
 }
